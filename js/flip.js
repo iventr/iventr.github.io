9cad598422caa0f5dbcab07200fc82b87a324fbb
Egor Rumyantsev <RumyantsevGT@ya.ru>
2018-12-11 07:13:14 +0300

    Create flip.js

---
+++ js/flip.js
@@ -0,0 +1,127 @@
+$('#flip-1').toggle(
+    function() {
+        $('#flip-1 .card').addClass('flipped');
+    },
+    function() { $('#flip-1 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-2').toggle(
+    function() {
+        $('#flip-2 .card').addClass('flipped');
+    },
+    function() { $('#flip-2 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-3').toggle(
+    function() {
+        $('#flip-3 .card').addClass('flipped');
+    },
+    function() { $('#flip-3 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-4').toggle(
+    function() {
+        $('#js-flip-4 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-4 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-5').toggle(
+    function() {
+        $('#js-flip-5 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-5 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-6').toggle(
+    function() {
+        $('#js-flip-6 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-6 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-7').toggle(
+    function() {
+        $('#js-flip-1 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-1 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-8').toggle(
+    function() {
+        $('#js-flip-1 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-1 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-9').toggle(
+    function() {
+        $('#js-flip-1 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-1 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-10').toggle(
+    function() {
+        $('#js-flip-1 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-1 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-11').toggle(
+    function() {
+        $('#js-flip-1 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-1 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-12').toggle(
+    function() {
+        $('#js-flip-1 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-1 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-13').toggle(
+    function() {
+        $('#js-flip-1 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-1 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-14').toggle(
+    function() {
+        $('#js-flip-1 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-1 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-15').toggle(
+    function() {
+        $('#js-flip-1 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-1 .card').removeClass('flipped');
+    }
+);
+
+$('#flip-16').toggle(
+    function() {
+        $('#js-flip-1 .card').addClass('flipped');
+    },
+    function() { $('#js-flip-1 .card').removeClass('flipped');
+    }
+);
